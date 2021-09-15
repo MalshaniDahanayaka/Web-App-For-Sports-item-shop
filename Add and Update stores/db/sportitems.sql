ccef6377db7035aaab3e56eb59985de39144b356
Kasun-se-2018-023 <skasunmk982@gmail.com>
2021-09-15 21:04:08 +0530

    update db

--- Add and Update stores/db/sportitems.sql
+++ Add and Update stores/db/sportitems.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 15, 2021 at 12:07 PM
+-- Generation Time: Sep 15, 2021 at 03:33 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -33,9 +33,10 @@ CREATE TABLE IF NOT EXISTS `items` (
   `sportName` varchar(40) NOT NULL,
   `productType` varchar(40) NOT NULL,
   `brand` varchar(20) NOT NULL,
-  `size` int(12) NOT NULL,
+  `size` varchar(12) NOT NULL,
   `prize` int(40) NOT NULL,
   `countOfItems` int(20) NOT NULL,
+  `category` varchar(20) NOT NULL,
   `imagePath` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
@@ -44,11 +45,12 @@ CREATE TABLE IF NOT EXISTS `items` (
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`id`, `sportName`, `productType`, `brand`, `size`, `prize`, `countOfItems`, `imagePath`) VALUES
-('CriBatnik4', 'Cricket', 'Bat', 'nike', 4, 34523, 6, 'img/9.jpg'),
-('VbaBalnik3', 'Vball', 'Ball', 'nike', 3, 456666, 5, 'img/8.jpg'),
-('BasBalMik1', 'Basket Ball', 'Ball', 'Mikasa', 1, 3456, 8, 'img/3.jpg'),
-('BatBatnik4', 'Batminton', 'Bat', 'nike', 4, 443353, 5, 'img/1.jpg');
+INSERT INTO `items` (`id`, `sportName`, `productType`, `brand`, `size`, `prize`, `countOfItems`, `category`, `imagePath`) VALUES
+('CriBatnik4', 'Cricket', 'Bat', 'nike', '4', 34523, 6, '', 'img/9.jpg'),
+('VbaBalnik3', 'Vball', 'Ball', 'nike', '3', 456666, 5, '', 'img/8.jpg'),
+('BasBalMik1', 'Basket Ball', 'Ball', 'Mikasa', '1', 3456, 8, '', 'img/3.jpg'),
+('BatBatnik4', 'Batminton', 'Bat', 'nike', '4', 443353, 5, '', 'img/1.jpg'),
+('BatBatnikmed', 'Batminton', 'Bat', 'nike', 'medium', 56777, 5, '', 'img/6.jpg');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
