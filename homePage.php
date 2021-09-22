31b475b517a718f5f77c03eb0e99f26953b86aad
Kasun-se-2018-023 <skasunmk982@gmail.com>
2021-09-22 13:15:53 +0530

    adding home page

---
+++ homePage.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Sports man</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+
+
+    <!-- Images slide show -->
+     <div class="container">
+ 
+               <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                         <!-- Indicators -->
+                         <ol class="carousel-indicators">
+                                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#myCarousel" data-slide-to="1"></li>
+                                <li data-target="#myCarousel" data-slide-to="2"></li>
+                         </ol>
+
+                         <!-- Wrapper for slides -->
+                         <div class="carousel-inner">
+                                  <div class="item active">
+                                        <img src="https://www.deccanherald.com/sites/dh/files/articleimages/2021/04/24/mithali-raj-pti-978019-1619256405.jpg" alt="Image 01" style="width:100%;">
+                                        <div class="carousel-caption d-none d-md-block">
+                                              <h2>This is Cricket </h2>
+                                               <p>this is team sport</p>
+                                        </div>
+                                   </div>
+      
+      
+
+                                   <div class="item">
+                                        <img src="https://metrifit.com/wp-content/uploads/2017/09/slide-05-mob-opt.jpg" alt="Image 02" style="width:100%;">
+                                        <div class="carousel-caption d-none d-md-block">
+                                              <h2>This is Football </h2>
+                                              <p>this is team sport</p>
+                                        </div>
+                                    </div>
+      
+      
+    
+                                    <div class="item">
+                                         <img src="https://www.teamusa.org/-/media/TeamUSA/Volleyball/Hill_Kimberly/Hill_Kim_052416_1440x810_Updated.png" alt="Image 03" style="width:100%;">
+                                         <div class="carousel-caption d-none d-md-block">
+                                               <h2>This is Volleyball </h2>
+                                               <p>this is team sport</p>
+                                         </div>
+                                    </div>	
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                    <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                    <span class="sr-only">Next</span>
+                        </a>
+                </div>
+     </div>
+
+
+
+
+<!-- Product Brands showing Bar -->
+<div class="container my-4" style="margin-top: 100px;">
+
+    <!--Grid row-->
+    <div class="row text-center">
+
+     <h1 style="font-weight: bold;margin-bottom: 50px;background-color:#8de4a0;">Brand Showing</h1>
+
+      <!--Grid column-->
+      <div class="col-12 col-md-12" >
+
+        <img src="https://www.hoopsstation.com/image/hoopsstation/image/data/2020/KET%202020/SBjo48jz1602506811.jpg" class="img-fluid mb-4" alt="" style="height: auto; width: 17rem;border-radius: 50%;  border: 4px solid #965807;margin-left:20px;margin-top:20px;" >
+        <img src="https://sonaesierracms-v2.cdnpservers.net/wp-content/uploads/sites/14/2019/04/e0e2edc1-1068-49a1-81ea-9eeb17845c68.adidas.jpg" class="img-fluid mb-4" alt="" style="height:auto; width: 17rem;border-radius: 50%;border: 4px solid #965807;margin-left:40px;margin-top:20px;">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXdQaUfo8-Kplmi7KF_kYi8UtzKf3WY_Fk0CYhCcTd9xEb6qwCXJBsBISAM3-83otUaN0&usqp=CAU" class="img-fluid mb-4" alt="" style="height: auto; width: 17rem;border-radius: 50%;border: 4px solid #965807;margin-left:40px;margin-top:20px;">
+        <img src="https://i.pinimg.com/originals/37/94/4a/37944a16706ed101106abe314cba5051.jpg" class="img-fluid mb-4" alt="" style="height:auto; width: 17rem;border-radius: 50%;border: 4px solid #965807;margin-left:40px;margin-top:20px;">
+        <img src="https://i.pinimg.com/originals/37/94/4a/37944a16706ed101106abe314cba5051.jpg" class="img-fluid mb-4" alt="" style="height: auto; width: 17rem;border-radius: 50%;border: 4px solid #965807;margin-left:40px;margin-top:20px;">
+        
+         </div>
+         <!--Grid column-->
+    </div>
+    <!--Grid row-->
+</div>
+
+
+
+
+</body>
+</html>
