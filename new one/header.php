6ccbf2dcfa5f0bedc8a174ad2342b711da25d3c6
Kasun-se-2018-023 <skasunmk982@gmail.com>
2021-09-26 11:22:58 +0530

    check header proble

---
+++ new one/header.php
@@ -0,0 +1,177 @@
+<link rel="stylesheet" type="text/css" href="include/header.css">
+ <style>
+        .navbar-nav.navbar-center {
+            position: absolute;
+            left: 50%;
+            
+            transform: translatex(-50%);
+        }
+    </style>
+
+<header class="header">
+<nav class="navbar navbar-style">
+	<div class="container">
+		<div class="navbar-header">
+			<a href=""> <img class="img-fluid" src="Sportsman1.png" ></a>
+		</div>
+        
+       <style>
+         
+          .containere {
+            position: relative;
+            text-align: center;
+          
+            }
+
+
+            .top-right {
+            position: absolute;
+            top: -8px;
+            right: -4px;
+            font-weight: bold;
+            border-radius: 50%;
+            width: 25px;
+            height: 25px;
+            background: yellow;
+            border: 3px solid blue;
+  
+            }
+            
+        </style>
+        
+        <ul class ="nav navbar-nav navbar-right containere">
+            <li> <a href=""><img class="img-fluid"src="cart.png" ></a></li>
+            <div class="top-right">5</div>
+        </ul>
+
+	</div>
+</nav>
+
+<style>
+
+
+.navbar2 {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.navbar2 a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.dropdown2 {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown2 .dropbtn2 {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar2 a:hover, .dropdown2:hover .dropbtn2 {
+  background-color: red;
+}
+
+.dropdown-content2 {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content2 a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content2 a:hover {
+  background-color: #ddd;
+}
+
+.dropdown2:hover .dropdown-content2 {
+  display: block;
+}
+
+
+</style>
+
+
+
+<form >
+<div class="navbar2" >
+  
+  <div class="dropdown2" >
+          <button class="dropbtn2" >Dropdown 
+                   <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content2"  style="margin-left: 500px;" >
+                   <a href="#">Link 1</a>
+                   <a href="#">Link 2</a>
+                   <a href="#">Link 3</a>
+          </div>
+  </div> 
+  <div class="dropdown2">
+          <button class="dropbtn2">Dropdown 
+                   <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content2">
+                   <a href="#">Link 1</a>
+                   <a href="#">Link 2</a>
+                   <a href="#">Link 3</a>
+          </div>
+  </div> 
+  <div class="dropdown2">
+          <button class="dropbtn2">Dropdown 
+                   <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content2">
+                   <a href="#">Link 1</a>
+                   <a href="#">Link 2</a>
+                   <a href="#">Link 3</a>
+          </div>
+  </div> 
+  <div class="dropdown2">
+          <button class="dropbtn2">Dropdown 
+                   <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content2">
+                   <a href="#">Link 1</a>
+                   <a href="#">Link 2</a>
+                   <a href="#">Link 3</a>
+          </div>
+  </div> 
+  <div class="dropdown2">
+          <button class="dropbtn2">Dropdown 
+                   <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content2">
+                   <a href="#">Link 1</a>
+                   <a href="#">Link 2</a>
+                   <a href="#">Link 3</a>
+          </div>
+  </div> 
+</div>
+
+</form>
+
+
+</header>
+
---
+++ new one/slideshow.php
@@ -0,0 +1,39 @@
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="margin-top: 185px;">
+          <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+<div class="carousel-inner">
+    <div class="carousel-item active">
+         <img class="d-block w-100" src="https://www.deccanherald.com/sites/dh/files/articleimages/2021/04/24/mithali-raj-pti-978019-1619256405.jpg" alt="First slide">
+         <div class="carousel-caption d-none d-md-block">
+           <h5>This is first picture</h5>
+           <p>it's working good i think</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="https://metrifit.com/wp-content/uploads/2017/09/slide-05-mob-opt.jpg" alt="Second slide">
+        <div class="carousel-caption d-none d-md-block">
+           <h5>This is second picture</h5>
+           <p>it's working good i think</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="https://www.teamusa.org/-/media/TeamUSA/Volleyball/Hill_Kimberly/Hill_Kim_052416_1440x810_Updated.png" alt="Third slide">
+        <div class="carousel-caption d-none d-md-block">
+           <h5>This is third picture</h5>
+           <p>it's working good i think</p>
+        </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+  </a>
+</div>
